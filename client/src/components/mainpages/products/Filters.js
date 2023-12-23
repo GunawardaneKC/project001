96121a75248a5eae84864af6aa1dca592aaa8131
Chamod S.G.K <kalanachamodac@gmail.com>
2023-12-23 23:22:16 +0530

    mobile view

--- client/src/components/mainpages/products/Filters.js
+++ client/src/components/mainpages/products/Filters.js
@@ -13,43 +13,35 @@ function Filters() {
   const handleCategory = (e) => {
     const selectedCategory = e.target.value;
     setCategory(selectedCategory);
-
-    // Fetch subcategories based on the selected category
     const selectedCategoryObject = categories.find((cat) => cat._id === selectedCategory);
     setSubcategories(selectedCategoryObject?.subcategory || []);
-
-    // Reset subcategory when changing the main category
     setSubcategory('');
   };
 
   return (
-    <>    
-   
+    <div className="mt-6 p-4 text-slate-900 rounded-2xl glass">
 
+      {/* Search Input */}
+      <div className="flex justify-center mb-4">
+        <input
+          type="text"
+          value={search}
+          placeholder="Search products here"
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          className="border rounded-md px-3 py-1 w-full text-center"
+        />
+      </div>
 
-    <div className="mt-6 p-4  text-slate-900 rounded-2xl glass">
-    <div className=" flex justify-center">
-          {/* Search Input */}
-          <input
-            type="text"
-            value={search}
-            placeholder="Search products Here"
-            onChange={(e) => setSearch(e.target.value.toLowerCase())}
-            className="border rounded-md px-3 py-1 w-5/6 text-center"
-          />
-        </div>
+      {/* Filter Section */}
+      <div className="flex flex-col md:flex-row">
+        <div className="font-medium">Filter by:</div>
 
-
-      <div className=" flex mt-3 gap-4 ">
-
-        <div className='font-medium'>Filter by: </div>
-
-        {/* Category Dropdown*/}
+        {/* Category Dropdown */}
         <select
           name="category"
           value={category}
           onChange={handleCategory}
-          className="border rounded-md px-2 py-1 w-44"
+          className="border rounded-md px-2 py-1 mb-2 md:mb-0 md:mr-2 w-full md:w-44"
         >
           <option value="">All Products</option>
           {categories.map((category) => (
@@ -59,13 +51,13 @@ function Filters() {
           ))}
         </select>
 
-        {/* Subcategory Dropdown */}
-        {subcategories.length > 0 && (
+        {/* Subcategory Section */}
+        {category !== "" && (
           <select
             name="subcategory"
             value={subcategory}
             onChange={(e) => setSubcategory(e.target.value)}
-            className="border rounded-md px-2 py-1 w-44"
+            className="border rounded-md px-2 py-1 mb-2 md:mb-0 md:mr-2 w-full md:w-44"
           >
             <option value="">All Subcategories</option>
             {subcategories.map((subcat) => (
@@ -75,36 +67,24 @@ function Filters() {
             ))}
           </select>
         )}
+    
 
         {/* Sort Section */}
-        <div className="ml-auto">
-        <span className="font-medium">Sort By: </span>
-        <select
-          value={sort}
-          onChange={(e) => setSort(e.target.value)}
-          className="border rounded-md px-2 py-1 w-44"
-        >
-          <option value="">Newest</option>
-          <option value="sort=oldest">Oldest</option>
-          {/* <option value='sort=-sold'>Best sales</option> */}
-          <option value="sort=-price">Price: High-Low</option>
-        </select>
+        <div className="flex flex-col md:flex-row ml-auto w">
+          <span className="font-medium mb-2 md:mb-0 md:mr-2">Sort By:</span>
+          <select
+            value={sort}
+            onChange={(e) => setSort(e.target.value)}
+            className="border rounded-md px-2 py-1 w-full md:w-44"
+          >
+            <option value="">Newest</option>
+            <option value="sort=oldest">Oldest</option>
+            <option value="sort=-price">Price: High-Low</option>
+          </select>
         </div>
-
-        
-
-
       </div>
-
-      
     </div>
-    </>
-
   );
 }
 
 export default Filters;
-
-
-
-
