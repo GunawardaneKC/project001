5a77609908865eacdb736fb2206888c6e612e670
GunawardaneKC <kavinduchanditha@gmail.com>
2024-01-05 19:14:40 +0530

    unwanted pages

--- client/src/components/mainpages/ContactUs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ContactUs() {
-  return (
-    <div className='text-black text-2xl'>ContactUs</div>
-  )
-}
--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -5,9 +5,7 @@ import DetailProduct from './detailProduct/DetailProduct';
 import Cart from './cart/Cart';
 import NotFound from './utils/not_found/NotFound';
 import Categories from './categories/Categories';
-import CreateProduct from './createProduct/CreateProduct';
-import ContactUs from './ContactUs';
-
+// import CreateProduct from './createProduct/CreateProduct';
 import {GlobalState} from '../../GlobalState';
 
 function Pages() {
@@ -22,13 +20,12 @@ function Pages() {
             <Route path="/detail/:id" exact element={<DetailProduct/>} />
 
             <Route path="/category" exact element={isAdmin ? <Categories/> : <NotFound/>} />
-            <Route path="/create_product" exact element={isAdmin ? <CreateProduct/> : <NotFound/>} />
-            <Route path="/edit_product/:id" exact element={isAdmin ? <CreateProduct/> : <NotFound/>} />
+            {/* <Route path="/create_product" exact element={isAdmin ? <CreateProduct/> : <NotFound/>} /> */}
+            {/* <Route path="/edit_product/:id" exact element={isAdmin ? <CreateProduct/> : <NotFound/>} /> */}
 
             <Route path="/cart" exact element={<Cart/>} />
 
             <Route path="*" exact element={<NotFound/>} />
-            <Route path="/contactus" exact element={<ContactUs/>} />
 
         </Routes>
         
