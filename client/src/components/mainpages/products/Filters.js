86f5af76f2d496dc792eccf6149a51881b1764b8
GunawardaneKC <kavinduchanditha@gmail.com>
2024-01-23 18:06:58 +0530

    up

--- client/src/components/mainpages/products/Filters.js
+++ client/src/components/mainpages/products/Filters.js
@@ -1,5 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { GlobalState } from '../../../GlobalState';
+import { styled } from '@mui/material/styles';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 function Filters() {
   const state = useContext(GlobalState);
@@ -28,6 +34,60 @@ function Filters() {
     const selectedCategory = e.target.value;
     setCategory(selectedCategory);
   };
+  
+  const BpIcon = styled('span')(({ theme }) => ({
+    borderRadius: '50%',
+    width: 16,
+    height: 16,
+    boxShadow:
+      theme.palette.mode === 'dark'
+        ? '0 0 0 1px rgb(16 22 26 / 40%)'
+        : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
+    backgroundImage:
+      theme.palette.mode === 'dark'
+        ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
+        : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    '.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
+    },
+    'input:hover ~ &': {
+      backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      background:
+        theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+    },
+  }));
+  
+  const BpCheckedIcon = styled(BpIcon)({
+    backgroundColor: '#137cbd',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    '&::before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+      content: '""',
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
+    },
+  });
+
+  function BpRadio(props) {
+    return (
+      <Radio
+        disableRipple
+        color="default"
+        checkedIcon={<BpCheckedIcon />}
+        icon={<BpIcon />}
+        {...props}
+      />
+    );
+  }
 
   return (
     <div className="mt-10 ml-4 mr-4 flex flex-wrap justify-between rounded-3xl p-4 bg-gray-200 text-slate-900">
@@ -65,7 +125,7 @@ function Filters() {
       )}
 
       {/* Radio buttons for condition */}
-      {category === 'Mobile Phones' && (
+      {/* {category === 'Mobile Phones' && (
         <div className="flex items-center gap-5 mt-1">
           <span className="font-medium">Condition:</span>
           <label style={{ display: 'flex', alignItems: 'center' }}>
@@ -89,7 +149,19 @@ function Filters() {
             Used
           </label>
         </div>
-      )}
+      )} */}
+
+<FormControl>
+      <FormLabel id="demo-customized-radios">Condition:</FormLabel>
+      <RadioGroup
+        defaultValue="female"
+        aria-labelledby="demo-customized-radios"
+        name="customized-radios"
+      >
+        <FormControlLabel value="brandnew" control={<BpRadio />} checked={condition === 'Brand New'} onChange={() => setCondition('Brand New')} label="Brand New" />
+        <FormControlLabel value="used" control={<BpRadio />} checked={condition === 'Used'} onChange={() => setCondition('Used')} label="Used" />
+      </RadioGroup>
+    </FormControl>
     </div>
 
       <div className="flex items-center gap-4">
