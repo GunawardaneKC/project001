a4ee3d288b93d523fa92fcdae80dd499238fd6bf
Gunawardane K.C. <kavinduchanditha@gmail.com>
2024-02-07 01:35:13 +0530

    maintanance

--- client/src/components/mainpages/maintain.js
+++ client/src/components/mainpages/maintain.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 function MaintenanceMessage() {
   return (
-    <div className='flex justify-center'>
-      <h1 className=' text-red-600 text-3xl'>Website Under Maintenance</h1>
-      <p className=' text-xl'>We apologize for the inconvenience. Please check back later.</p>
+    <div className=' min-h-[600px]'>
+
+    <div className='flex justify-center mt-10'>
+      <h1 className=' text-red-600 text-3xl'>Website Under Maintenance</h1> 
+    </div>
+      <div className='flex justify-center mt-10'>
+      <p className=' text-xl text-black'>We apologize for the inconvenience. Please check back later.</p>
+      </div>
     </div>
   );
 }
