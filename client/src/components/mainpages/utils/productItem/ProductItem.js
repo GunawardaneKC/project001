8108d37265dd5007fdd72ef3983ab2b6cb99e6d3
Gunawardane K.C. <kavinduchanditha@gmail.com>
2024-02-12 15:19:48 +0530

    ...

--- client/src/components/mainpages/utils/productItem/ProductItem.js
+++ client/src/components/mainpages/utils/productItem/ProductItem.js
@@ -30,67 +30,65 @@ function ProductItem({ product }) {
   return (
     <div className="m-4">
       <Link to={`/detail/${product._id}`}>
-      <Card
-  hoverable
-  className="hover-card" 
-  style={{ width: 240 }}
-  cover={
-    <div
-      className="hover-zoom relative" // Added relative class for positioning of discount price
-      style={{
-        width: '100%',
-        height: '15rem',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: 'hidden',
-      }}
-    >
-      <img
-        alt={product.title}
-        src={product.images.url}
-        className="zoom-image"
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
-          transition: 'transform 0.8s ease-in-out',
-        }}
-      />
+        <div className="hover-card-container">
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <div
+                className="hover-zoom relative" // Added relative class for positioning of discount price
+                style={{
+                  width: '100%',
+                  height: '15rem',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  overflow: 'hidden',
+                }}
+              >
+                <img
+                  alt={product.title}
+                  src={product.images.url}
+                  className="zoom-image"
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                  }}
+                />
 
-      {product.discountprice && (
-        <div style={discountRibbonStyles.discountRibbon}>
-          <p style={discountRibbonStyles.ribbonText}>
-            {`Rs ${Number(product.discountprice).toLocaleString()} /=`}
-          </p>
-        </div>
-      )}
-    </div>
-  }
->
-          <Card.Meta
-            className="text-center text-xl font-semibold"
-            title={product.title}
-            description={
-              <span style={{ color: 'blue' }}>
-                {`Rs: ${Number(product.price).toLocaleString()}/=`}
-              </span>
+                {product.discountprice && (
+                  <div style={discountRibbonStyles.discountRibbon}>
+                    <p style={discountRibbonStyles.ribbonText}>
+                      {`Rs ${Number(product.discountprice).toLocaleString()} /=`}
+                    </p>
+                  </div>
+                )}
+              </div>
             }
-          />
+          >
+            <Card.Meta
+              className="text-center text-xl font-semibold"
+              title={product.title}
+              description={
+                <span style={{ color: 'blue' }}>
+                  {`Rs: ${Number(product.price).toLocaleString()}/=`}
+                </span>
+              }
+            />
 
-          
-
-          {product.description && (
-            <p className="text-red-500 font-bold mt-2 text-center h-7 bg-opacity-75">
-              {product.description}
-            </p>
-          )}
-          {product.stock && (
-            <p className="text-red-500 font-bold bg-slate-300 mt-2 text-center h-7 bg-opacity-75">
-              {product.stock}
-            </p>
-          )}
-        </Card>
+            {product.description && (
+              <p className="text-red-500 font-bold mt-2 text-center h-7 bg-opacity-75">
+                {product.description}
+              </p>
+            )}
+            {product.stock && (
+              <p className="text-red-500 font-bold bg-slate-300 mt-2 text-center h-7 bg-opacity-75">
+                {product.stock}
+              </p>
+            )}
+          </Card>
+        </div>
       </Link>
     </div>
   );
