c7292d9b714a0cfbdb5b273210162290832b54cc
Gunawardane K.C. <kavinduchanditha@gmail.com>
2024-03-04 15:30:10 +0530

    up

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -120,7 +120,7 @@ export default function Footer() {
       <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
       </svg>
     </a>
-    <a className="mr-6 text-neutral-600 dark:text-neutral-200" href='https://wa.me/94778743175'>
+    <a className="mr-6 text-neutral-600 dark:text-neutral-200" href='https://wa.me/94770110001'>
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       width="32" height="32" 
