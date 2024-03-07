c8a3ac0bd2c91ddacc6256c0d8eb53a30949a61c
Gunawardane K.C. <kavinduchanditha@gmail.com>
2024-03-07 14:00:43 +0530

    up

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -76,7 +76,7 @@ function DetailProduct() {
                 </div>
                )}
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <a href={`https://api.whatsapp.com/send?Phone=+94778743175&text=Hi,%20I%20want%20to%20order%20this%20product:%0A${'Item: ' + detailProduct.title}%0A${'Model: ' + detailProduct.description}%0A${'Price: ' + detailProduct.price}%0A${detailProduct.discountprice ? 'Discount price: ' + detailProduct.discountprice : ''}%0A%0APage URL: ${window.location.href}`} style={{ textDecoration: 'none' }}>
+                <a href={`https://api.whatsapp.com/send?Phone=+94778743175&text=Hi,%20I%20want%20to%20order%20this%20product;%0A${'Item: ' + detailProduct.title}%0A${'Model: ' + detailProduct.description}%0A${'Price: ' + detailProduct.price}%0A${detailProduct.discountprice ? 'Discount price: ' + detailProduct.discountprice : ''}%0A%0APage URL: ${window.location.href}`} style={{ textDecoration: 'none' }}>
                   <div style={{ marginBottom: '10px', padding: '9px', textAlign: 'center', backgroundColor: '#1EA651', height: '44px', width: '250px', color: 'white', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                     <span style={{ fontWeight: 'bold' }}>Order On WhatsApp</span>
                     <div className='ml-0' style={{ marginLeft: '5px' }}>
